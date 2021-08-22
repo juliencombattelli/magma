6ba412e22bb96eae3b55cc2117339e70856c351f
Julien Combattelli <julien.combattelli@gmail.com>
2021-08-22 22:00:12 +0200

    Add CMake-generated version header

--- include/magma/EngineInfo.hpp
+++ include/magma/EngineInfo.hpp
@@ -1,13 +1,15 @@
 #pragma once
 
+#include <magma/Version.hpp>
+
 #include <vulkan/vulkan_core.h>
 
 namespace magma {
 
 struct EngineInfo {
     static constexpr inline const char* name = "Magma";
-    // TODO generate from CMake
-    static constexpr inline uint32_t version = VK_MAKE_API_VERSION(0, 1, 0, 0);
+    static constexpr inline uint32_t version
+        = VK_MAKE_API_VERSION(0, Version::major, Version::minor, Version::patch);
 };
 
 } // namespace magma
