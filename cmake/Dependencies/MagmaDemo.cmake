f869bf87935efe5a041893b6e1f26e9a5b2f6225
Julien Combattelli <julien.combattelli@gmail.com>
2021-08-28 21:03:35 +0200

    Add download and compilation of MagmaDemo

---
+++ cmake/Dependencies/MagmaDemo.cmake
@@ -0,0 +1,12 @@
+# FetchContent_MakeAvailable was added in CMake 3.14
+cmake_minimum_required(VERSION 3.14)
+
+include(FetchContent)
+
+FetchContent_Declare(
+    magma-demo
+    GIT_REPOSITORY  git@github.com:juliencombattelli/magma-demo
+    GIT_TAG         main
+)
+
+FetchContent_MakeAvailable(magma-demo)
